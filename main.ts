d6ae44d2aa253b71d4e6808200284b7237b3e2b4
HenwyHenwy <119698680+HenwyHenwy@users.noreply.github.com>
2022-12-02 19:50:33 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hello")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+basic.forever(function () {
+	
+})
